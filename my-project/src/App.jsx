904facb70b2113cbea78fa3042f82e8bf30d80a9
AlfredDuc <anhduc20053110@gmail.com>
2024-12-19 13:45:11 +0700

    Config view ui

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="main">Hello world</div>;
+  return (
+    <div className="main">
+      <video className="video" autoPlay />
+      <div className="control">
+        <button className="btn">Train 1</button>
+        <button className="btn">Train 2</button>
+        <button className="btn">Run</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
