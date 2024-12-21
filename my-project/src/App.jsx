c817600aea1ee0d504b22650d7d103a63d13a09f
AlfredDuc <anhduc20053110@gmail.com>
2024-12-21 20:08:00 +0700

    Train function

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@
 import "./App.css";
 import { useEffect, useRef } from "react";
 import { Howl } from "howler";
+import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import * as knnClassifier from "@tensorflow-models/knn-classifier";
 import soundUrl from "./assets/hey_tay.mp3";
@@ -10,21 +11,30 @@ var sound = new Howl({
 });
 // sound.play();
 
-var start;
-
 function App() {
+  // const variable
+  const NOT_TOUCH_MODULE = "not_touch";
+  const TOUCHED = "touched";
+  const TRAINING_TIMES = 50;
   useEffect(() => {
     init();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const video = useRef();
 
+  const video = useRef();
+  let classifierModule = useRef();
+  let mobilenetModule = useRef();
   const init = async () => {
     console.log("init...");
 
     await setUpCamera();
-
     console.log("setUp camera success");
+
+    classifierModule.current = knnClassifier.create();
+    mobilenetModule.current = await mobilenet.load();
+
+    console.log("set up done");
+    console.log("Dont touch face and press Train 1 ");
   };
 
   const setUpCamera = () => {
@@ -50,13 +60,31 @@ function App() {
     });
   };
 
+  const train = async (label) => {
+    for (let i = 0; i < TRAINING_TIMES; i++) {
+      console.log(`Progress ${parseInt(((i + 1) / TRAINING_TIMES) * 100)}%`);
+      await sleep(100);
+    }
+  };
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
   return (
     <div className="main">
       <video ref={video} className="video" autoPlay />
       <div className="control">
-        <button className="btn">Train 1</button>
-        <button className="btn">Train 2</button>
-        <button className="btn">Run</button>
+        <button onClick={() => train(NOT_TOUCH_MODULE)} className="btn">
+          Train 1
+        </button>
+        <button onClick={() => train(TOUCHED)} className="btn">
+          Train 2
+        </button>
+        <button onClick={() => {}} className="btn">
+          Run
+        </button>
       </div>
     </div>
   );
